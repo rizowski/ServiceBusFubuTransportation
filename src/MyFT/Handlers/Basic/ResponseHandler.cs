f5cd3d5db62b8bf73940bcf40cd34dc159ed6999
Colten Rouska <colten.rouska@extendhealth.com>
2015-04-23 15:26:08 -0600

    Added extra info

--- src/MyFT/Handlers/Basic/ResponseHandler.cs
+++ src/MyFT/Handlers/Basic/ResponseHandler.cs
@@ -9,9 +9,9 @@ namespace MyFT.Handlers.Basic
         {
             Console.WriteLine("Received from client: {0}", request.Type);
             var message = request.Type == MessageType.Hello ? "Hi Thar!" : "Go away! :D";
-            
+
             //throw new Exception();
-            
+
             return new ResponseMessage
             {
                 Message = message
--- src/MyFT/Policy.cs
+++ src/MyFT/Policy.cs
@@ -8,6 +8,10 @@ namespace MyFT
         public override void Configure(HandlerChain handlerChain)
         {
             handlerChain.MaximumAttempts = 5;
+
+            //handlerChain.OnException<Exception>()
+            //    .MoveToErrorQueue(); // For Diagnostics / Can be subscribed to?
+
             handlerChain.OnException<Exception>()
                 .Retry();  //.Requeue
         }
